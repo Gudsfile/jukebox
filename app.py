10db0dc8611489a4bd676ca5dab146636478417d
Gudsfile <gudsfile@protonmail.com>
2024-08-14 22:22:19 +0200

    ✨ allow shuffle mode

--- app.py
+++ app.py
@@ -8,9 +8,13 @@ from soco import SoCo
 from soco.plugins.sharelink import ShareLinkPlugin
 
 
-def play(sharelink, uri):
+def play(sharelink, uri, shuffle):
     sharelink.soco.clear_queue()
-    _ = sharelink.add_share_link_to_queue(uri, position=1, as_next=True)
+    _ = sharelink.add_share_link_to_queue(uri, position=1)
+    if shuffle:
+        sharelink.soco.play_mode = "SHUFFLE_NOREPEAT"
+    else:
+        sharelink.soco.play_mode = "NORMAL"
     sharelink.soco.play_from_queue(index=0, start=True)
 
 
@@ -31,6 +35,7 @@ def get_args():
     play_parser = subparsers.add_parser("play", help="play specific songs")
     play_parser.add_argument("--artist", required=True, help="specify the artist name to play")
     play_parser.add_argument("--album", required=True, help="specify the album name to play")
+    play_parser.add_argument("--shuffle", action="store_true", help="turns on shuffle")
     _ = subparsers.add_parser("list", help="list library contents")
     _ = subparsers.add_parser("stop", help="stop music and clear queue")
     return parser.parse_args()
@@ -54,7 +59,7 @@ def main():
             sonos_host = get_env()
             sharelink = create_speaker(sonos_host)
             uri = library[args.artist][args.album]
-            play(sharelink, uri)
+            play(sharelink, uri, args.shuffle)
         case "stop":
             sonos_host = get_env()
             sharelink = create_speaker(sonos_host)
