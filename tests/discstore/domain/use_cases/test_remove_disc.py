c6ce59dee65e984f6bfba661431d318934066dc2
Gudsfile <gudsfile@protonmail.com>
2025-08-10 13:32:48 +0200

    ♻refactor use_cases tests

--- tests/discstore/domain/use_cases/test_remove_disc.py
+++ tests/discstore/domain/use_cases/test_remove_disc.py
@@ -11,7 +11,7 @@ from discstore.domain.use_cases.remove_disc import RemoveDisc
 class FakeRepo(LibraryRepository):
     def __init__(self):
         self.saved_library: Optional[Library] = None
-        self.library = Library(discs={"tag42": Disc(uri="/existing.mp3", metadata=DiscMetadata())})
+        self.library = Library(discs={"existing-tag": Disc(uri="/existing.mp3", metadata=DiscMetadata())})
 
     def load(self):
         return self.library
@@ -20,22 +20,21 @@ class FakeRepo(LibraryRepository):
         self.saved_library = library
 
 
-def test_remove_disc_adds_disc():
+def test_remove_disc_removes_disc():
     repo = FakeRepo()
     use_case = RemoveDisc(repo)
 
-    use_case.execute("tag42")
+    use_case.execute("existing-tag")
 
     assert repo.saved_library is not None
-    assert "tag42" not in repo.saved_library.discs
     assert repo.saved_library.discs == {}
 
 
-def test_remove_disc_fails_if_tag_exists():
+def test_remove_disc_fails_if_tag_does_not_exists():
     repo = FakeRepo()
     use_case = RemoveDisc(repo)
 
     with pytest.raises(ValueError) as exc:
-        use_case.execute("tag44")
+        use_case.execute("non-existing-tag")
 
-    assert "Tag does not exist: tag_id='tag44'" in str(exc.value)
+    assert "Tag does not exist: tag_id='non-existing-tag'" in str(exc.value)
