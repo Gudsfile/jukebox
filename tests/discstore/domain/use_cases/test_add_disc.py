85a1fd86ca01509302def852208c28c7285eeafa
Gudsfile <gudsfile@protonmail.com>
2025-08-10 13:32:48 +0200

    ♻refactor use_cases tests

--- tests/discstore/domain/use_cases/test_add_disc.py
+++ tests/discstore/domain/use_cases/test_add_disc.py
@@ -11,7 +11,7 @@ from discstore.domain.use_cases.add_disc import AddDisc
 class FakeRepo(LibraryRepository):
     def __init__(self):
         self.saved_library: Optional[Library] = None
-        self.library = Library(discs={})
+        self.library = Library(discs={"existing-tag": Disc(uri="/existing.mp3", metadata=DiscMetadata())})
 
     def load(self):
         return self.library
@@ -24,23 +24,24 @@ def test_add_disc_adds_disc():
     repo = FakeRepo()
     use_case = AddDisc(repo)
 
-    disc = Disc(uri="/music.mp3", metadata=DiscMetadata())
-    use_case.execute("tag42", disc)
+    new_disc = Disc(uri="/new.mp3", metadata=DiscMetadata())
+    use_case.execute("non-existing-tag", new_disc)
 
     assert repo.saved_library is not None
-    assert "tag42" in repo.saved_library.discs
-    assert repo.saved_library.discs["tag42"] == disc
+    assert len(repo.saved_library.discs) == 2
+    assert "existing-tag" in repo.saved_library.discs
+    assert repo.saved_library.discs["existing-tag"] != new_disc
+    assert "non-existing-tag" in repo.saved_library.discs
+    assert repo.saved_library.discs["non-existing-tag"] == new_disc
 
 
 def test_add_disc_fails_if_tag_exists():
     repo = FakeRepo()
-    existing_disc = Disc(uri="/existing.mp3", metadata=DiscMetadata())
-    repo.library.discs["tag42"] = existing_disc
-
     use_case = AddDisc(repo)
+
     new_disc = Disc(uri="/new.mp3", metadata=DiscMetadata())
-
     with pytest.raises(ValueError) as exc:
-        use_case.execute("tag42", new_disc)
+        use_case.execute("existing-tag", new_disc)
 
-    assert "Already existing tag: tag_id='tag42'" in str(exc.value)
+    assert "Already existing tag: tag_id='existing-tag'" in str(exc.value)
+    assert repo.saved_library is None
