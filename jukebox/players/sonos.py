9e39353b280cc8b6d947b9a40049c77e01d998d6
Gudsfile <gudsfile@protonmail.com>
2025-08-12 20:11:15 +0200

    🥅 catching known SoCo UPnP exceptions

--- jukebox/players/sonos.py
+++ jukebox/players/sonos.py
@@ -3,11 +3,30 @@ import os
 from typing import Union
 
 from soco import SoCo
+from soco.exceptions import SoCoUPnPException
 from soco.plugins.sharelink import ShareLinkPlugin
 
 from .player import Player
 
 LOGGER = logging.getLogger("jukebox")
+
+
+def catch_soco_upnp_exception(func):
+    def wrapper(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except SoCoUPnPException as err:
+            if "UPnP Error 804" in str(err.message):
+                LOGGER.warning(f"{func.__name__} with `{args}` failed, probably a bad uri: {str(err.message)}")
+            elif "UPnP Error 701" in str(err.message):
+                LOGGER.warning(
+                    f"{func.__name__} with `{args}` failed, probably a not available transition: {str(err.message)}"
+                )
+            else:
+                LOGGER.error(f"{func.__name__} with `{args}` failed", err)
+            return
+
+    return wrapper
 
 
 class SonosPlayer(Player):
@@ -22,6 +41,7 @@ class SonosPlayer(Player):
         )
         self.sharelink = ShareLinkPlugin(self.speaker)
 
+    @catch_soco_upnp_exception
     def play(self, uri: str, shuffle: bool):
         LOGGER.info(f"Playing `{uri}` on the player `{self.speaker.player_name}`")
         self.speaker.clear_queue()
@@ -29,14 +49,17 @@ class SonosPlayer(Player):
         self.speaker.play_mode = "SHUFFLE_NOREPEAT" if shuffle else "NORMAL"
         self.speaker.play_from_queue(index=0, start=True)
 
+    @catch_soco_upnp_exception
     def pause(self):
         LOGGER.info(f"Pausing player `{self.speaker.player_name}`")
         self.speaker.pause()
 
+    @catch_soco_upnp_exception
     def resume(self):
         LOGGER.info(f"Resuming player `{self.speaker.player_name}`")
         self.speaker.play()
 
+    @catch_soco_upnp_exception
     def stop(self):
         LOGGER.info(f"Stopping player `{self.speaker.player_name}` and clearing its queue")
         self.speaker.clear_queue()
