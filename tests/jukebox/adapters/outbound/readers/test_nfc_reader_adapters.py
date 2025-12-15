e541117e7eb6e75a9a91f3b5535347f47affc705
Gudsfile <gudsfile@protonmail.com>
2025-12-15 22:06:50 +0100

    🚨 fix unresolved-attribute type-checker error

--- tests/jukebox/adapters/outbound/readers/test_nfc_reader_adapters.py
+++ tests/jukebox/adapters/outbound/readers/test_nfc_reader_adapters.py
@@ -5,12 +5,8 @@ import pytest
 
 @pytest.fixture()
 def mock_pn532_lib_installed():
-    def lib_func():
-        print("lib_func called")
-
     module = type(sys)("pn532")
     module.submodule = type(sys)("PN532_SPI")
-    module.submodule.lib_func = lib_func
     sys.modules["pn532"] = module
     sys.modules["pn532.PN532_SPI"] = module.submodule
 
