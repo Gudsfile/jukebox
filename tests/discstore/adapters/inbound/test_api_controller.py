6324232b32fa4ef22b706bdecc72767637320041
Gudsfile <gudsfile@protonmail.com>
2025-12-16 00:02:40 +0100

    🚨 fix multiple-with-statements linter error

--- tests/discstore/adapters/inbound/test_api_controller.py
+++ tests/discstore/adapters/inbound/test_api_controller.py
@@ -6,9 +6,8 @@ import pytest
 
 def test_module_import_failure():
     version_below_py37 = (3, 7, 17, "final", 0)
-    with mock.patch("sys.version_info", version_below_py37):
-        with pytest.raises(RuntimeError) as err:
-            import discstore.adapters.inbound.api_controller  # noqa: F401
+    with mock.patch("sys.version_info", version_below_py37), pytest.raises(RuntimeError) as err:
+        import discstore.adapters.inbound.api_controller  # noqa: F401
 
     assert "The `api_controller` module requires Python 3.8+." in str(err.value)
 
