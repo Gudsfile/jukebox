8aac6010c7541c3f0d4b833495032b8c9f78c3c4
Michael Gerbush <405526+msgerbush@users.noreply.github.com>
2026-03-19 23:22:00 -0400

    Fix lint issue

--- discstore/domain/entities/__init__.py
+++ discstore/domain/entities/__init__.py
@@ -1,5 +1,6 @@
-from .current_tag_status import CurrentTagStatus
 from jukebox.domain.entities.disc import Disc, DiscMetadata, DiscOption
 from jukebox.domain.entities.library import Library
+
+from .current_tag_status import CurrentTagStatus
 
 __all__ = ["CurrentTagStatus", "Disc", "DiscMetadata", "DiscOption", "Library"]
