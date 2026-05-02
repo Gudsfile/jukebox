81d5cfabe94662b2d95140376ef557fbf68207c3
Gudsfile <gudsfile@protonmail.com>
2026-05-02 18:46:18 +0200

    ♻️ move library use cases from discstore to jukebox domain

---
+++ jukebox/domain/use_cases/library/get_disc.py
@@ -0,0 +1,14 @@
+from jukebox.domain.entities import Disc
+from jukebox.domain.repositories import LibraryRepository
+
+
+class GetDisc:
+    def __init__(self, repository: LibraryRepository):
+        self.repository = repository
+
+    def execute(self, tag_id: str) -> Disc:
+        disc = self.repository.get_disc(tag_id)
+        if disc is None:
+            raise ValueError(f"Tag not found: tag_id='{tag_id}'")
+
+        return disc
